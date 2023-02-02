95fe7f3564bf6fb7d11d6247da53aa0b5b6cb57b
zhangjie <zhangjie@tcxa.com.cn>
2023-02-02 15:29:57 +0800

    fix: http timeout 50s

--- src/utils/http/config.ts
+++ src/utils/http/config.ts
@@ -2,7 +2,7 @@ import { AxiosRequestConfig } from 'axios'
 
 const defaultConfig: AxiosRequestConfig = {
   baseURL: process.env.VUE_APP_BASE_API, // api的base_url
-  timeout: 10000, // request timeout
+  timeout: 50 * 1000, // request timeout
   headers: {
     Accept: 'application/json, text/plain, */*; charset=utf-8',
     'Content-Type': 'application/json; charset=utf-8',
